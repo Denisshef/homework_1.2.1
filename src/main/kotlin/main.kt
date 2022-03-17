da3cc464bf404be2e7a5b66e681e80ae09580596
Denisshef <denisshef24@yandex.ru>
2022-03-17 14:31:15 +0700

    Home work 1.3.2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,47 @@
+var amountTransferPastMonth = 50000
+
+enum class Card {
+    MASTERCARD, MAESTRO, VISA, MIR, VKPAY
+}
+
 fun main() {
-    val taxCommission = 75
+    userInput()
+}
+
+fun userInput() {
+    var card: Card
     var amount: Int
+
+    while (true) {
+        print(
+            """
+            Откуда совершаете платёж?
+            1 Mastercard
+            2 Maestro
+            3 Visa
+            4 Мир 
+            5 VK Pay
+            укажите номер (1 - 5): 
+        """.trimIndent()
+        )
+        try {
+            val userNumberCard = readLine()?.toInt() ?: return
+            if (userNumberCard < 1 || userNumberCard > 5) {
+                println("Выберете от 1 до 5!\n")
+                continue
+            }
+            card = when (userNumberCard) {
+                1 -> Card.MASTERCARD
+                2 -> Card.MAESTRO
+                3 -> Card.VISA
+                4 -> Card.MIR
+                else -> Card.VKPAY
+            }
+            break
+        } catch (err: Exception) {
+            println("Странный ввод :)\n")
+        }
+    }
 
     while (true) {
         print("Введите сумму перевода: ")
@@ -15,11 +56,31 @@ fun main() {
             println("!Не похоже на сумму :)\n")
         }
     }
-    println("Сумма перевода $amount руб.")
-    amount = (amount * taxCommission) / 10000
-    if (amount <= 35) {
-        print("Сумма комиссии за перевод 35 руб.")
-    } else {
-        print("Сумма комиссии за перевод $amount руб.")
+    transfer(card, amount * 100)
+}
+
+
+fun transfer(card: Card, amount: Int) {
+    val str = when (card) {
+        Card.MASTERCARD, Card.MAESTRO -> {
+            if (amountTransferPastMonth in 30000..7500000) {
+                "0"
+            } else {
+                " ${((amount * 0.006) + 2000) / 100}"
+            }
+        }
+        Card.VISA, Card.MIR -> {
+            if ((amount * 0.0075) < 3500) {
+                "35"
+            } else {
+                "${(amount * 0.0075) / 100}"
+            }
+        }
+        else -> "0"
     }
+    amountTransferPastMonth += amount
+
+    println("--> перевод с $card")
+    println("--> сумма перевода ${amount / 100} руб.")
+    println("--> комиссия за перевод $str руб.")
 }
